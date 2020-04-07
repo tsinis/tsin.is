22cad87addf464072871552c65a40cc04d02c8a7
Cinis Roman <52065414+tsinis@users.noreply.github.com>
2020-04-07 14:27:34 +0200

    Code Cleaning in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,6 @@ class _MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<_MyHomePage> {
-  static final scrollDirection = Axis.vertical;
-
   ScrollController scrollController;
 
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -68,139 +66,132 @@ class _MyHomePageState extends State<_MyHomePage> {
       scrollController.hasClients ? scrollController.offset : 0;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      endDrawer: SideMenu(scrollController),
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            top: -0.3 * offset,
-            left: 0,
-            right: 0,
-            height: height,
-            child: RepaintBoundary(
-              child: AnimatedBackground(),
-            ),
-          ),
-          Positioned(
-            top: 0.2 * height,
-            left: 0,
-            right: 0,
-            child: MainText(offset),
-          ),
-          Positioned(
-            top: height * 0.8 - offset,
-            left: 0,
-            right: 0,
-            height: height * 0.2,
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  stops: [0, 0.8],
-                  colors: [_grey.withOpacity(0), _grey],
-                ),
+  Widget build(BuildContext context) => Scaffold(
+        key: _scaffoldKey,
+        endDrawer: SideMenu(scrollController),
+        body: Stack(
+          children: <Widget>[
+            Positioned(
+              top: -0.3 * offset,
+              left: 0,
+              right: 0,
+              height: height,
+              child: RepaintBoundary(
+                child: AnimatedBackground(),
               ),
             ),
-          ),
-          Positioned(
-            top: height * 0.95 - offset,
-            left: 0,
-            right: 0,
-            height: height / 3.0,
-            child: Container(
-              height: height / 3.0,
-              width: double.infinity,
-              color: _grey,
+            Positioned(
+              top: 0.2 * height,
+              left: 0,
+              right: 0,
+              child: MainText(offset),
             ),
-          ),
-          ListView(
-            scrollDirection: scrollDirection,
-            cacheExtent: double.infinity,
-            controller: scrollController,
-            children: <Widget>[
-              Container(height: height),
-              Container(
-                height: 100.0,
-                color: _grey,
-              ),
-              Container(
-                color: _grey,
-                child: About(),
-              ),
-              Portfolio(),
-              Contact(),
-              Footer()
-            ],
-          ),
-          Positioned(
-            right: -(height / 3.0),
-            top: height / 2.0,
-            height: 2.0,
-            width: height * 0.75,
-            child: scrollController.hasClients
-                ? ScrollProgress(
-                    height: height,
-                    offset: offset,
-                    scrollController: scrollController)
-                : Container(),
-          ),
-          (height > MediaQuery.of(context).size.width)
-              ? Container()
-              : Positioned(
-                  left: 80,
-                  bottom: 70,
-                  child: CircularText(
-                    textStyle: Theme.of(context).textTheme.overline.copyWith(
-                          fontSize: 10,
-                          color: Colors.white.withOpacity(
-                            min(offset / 1000, 0.3),
-                          ),
-                        ),
-                    startAngle: -pi / 2.0 + offset / 500,
+            Positioned(
+              top: height * 0.8 - offset,
+              left: 0,
+              right: 0,
+              height: height * 0.2,
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0, 0.8],
+                    colors: [_grey.withOpacity(0), _grey],
                   ),
                 ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Opacity(
-                opacity: max(0, 1.0 - offset / height),
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      top: 30.0, left: _isSmartPhone ? 30.0 : 50.0),
-                  child: LanguageMenu(
-                    isSmartphone: _isSmartPhone,
-                    language: S.of(context).language,
-                    tooltip: S.of(context).selectLang,
-                    onSelected: (String value) => setState(() {
-                      S.load(Locale(value));
-                    }),
-                  ).showCursorOnHover,
-                ),
               ),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: 30.0, right: _isSmartPhone ? 20.0 : 50.0),
-                child: _isSmartPhone
-                    ? IconButton(
-                        icon: Icon(Icons.menu),
-                        color: Theme.of(context).accentColor,
-                        onPressed: () {
-                          _scaffoldKey.currentState.openEndDrawer();
-                        },
-                      ).showCursorOnHover.moveUpOnHover
-                    : Opacity(
-                        opacity: max(0, 1.0 - offset / height),
-                        child: Header(scrollController),
-                      ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
+            ),
+            Positioned(
+              top: height * 0.95 - offset,
+              left: 0,
+              right: 0,
+              height: height / 3.0,
+              child: Container(
+                height: height / 3.0,
+                width: double.infinity,
+                color: _grey,
+              ),
+            ),
+            ListView(
+              scrollDirection: Axis.vertical,
+              cacheExtent: double.infinity,
+              controller: scrollController,
+              children: <Widget>[
+                Container(height: height),
+                Container(
+                  color: _grey,
+                  child: About(),
+                ),
+                Portfolio(),
+                Contact(),
+                Footer()
+              ],
+            ),
+            Positioned(
+              right: -(height / 3.0),
+              top: height / 2.0,
+              height: 2.0,
+              width: height * 0.75,
+              child: scrollController.hasClients
+                  ? ScrollProgress(
+                      height: height,
+                      offset: offset,
+                      scrollController: scrollController)
+                  : Container(),
+            ),
+            (height > MediaQuery.of(context).size.width)
+                ? Container()
+                : Positioned(
+                    left: 80.0,
+                    bottom: 70.0,
+                    child: CircularText(
+                      textStyle: Theme.of(context).textTheme.overline.copyWith(
+                            fontSize: 10.0,
+                            color: Colors.white.withOpacity(
+                              min(offset / 1000.0, 0.3),
+                            ),
+                          ),
+                      startAngle: -pi / 2.0 + offset / 500.0,
+                    ),
+                  ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Opacity(
+                  opacity: max(0, 1.0 - offset / height),
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        top: 30.0, left: _isSmartPhone ? 30.0 : 50.0),
+                    child: LanguageMenu(
+                      isSmartphone: _isSmartPhone,
+                      language: S.of(context).language,
+                      tooltip: S.of(context).selectLang,
+                      onSelected: (String _selectedLand) => setState(() {
+                        S.load(Locale(_selectedLand));
+                      }),
+                    ).showCursorOnHover,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: 30.0, right: _isSmartPhone ? 20.0 : 50.0),
+                  child: _isSmartPhone
+                      ? IconButton(
+                          icon: Icon(Icons.menu),
+                          color: Theme.of(context).accentColor,
+                          onPressed: () =>
+                              _scaffoldKey.currentState.openEndDrawer(),
+                        ).showCursorOnHover.moveUpOnHover
+                      : Opacity(
+                          opacity: max(0, 1.0 - offset / height),
+                          child: Header(scrollController),
+                        ),
+                )
+              ],
+            )
+          ],
+        ),
+      );
 }
