191e96847f99f5c8f0217ca0a9efb1892a12efcd
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2020-05-02 19:54:21 +0200

    Added Zoom on Cursor Hover in Portfolio

--- lib/animations/zoom_on_hover.dart
+++ lib/animations/zoom_on_hover.dart
@@ -13,32 +13,45 @@ class _TranslateOnHoverState extends State<ZoomOnHover>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
   bool _hovering = false;
+  Animation<double> _animationCurve;
 
   @override
   void initState() {
     super.initState();
-    _controller =
-        AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _animationCurve = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOutQuint,
+    );
+    _controller.forward();
   }
-  // double get height => MediaQuery.of(context).size.height;
 
-  final _hoverTransform = Matrix4.identity()..scale(1.1, 1.1);
-  final _nonHoverTransform = Matrix4.identity();
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   void _mouseEnter(bool hovering) => setState(() => _hovering = hovering);
 
   @override
   Widget build(BuildContext context) {
+    final _animation = Tween<double>(begin: 0, end: 1).animate(_animationCurve);
     return MouseRegion(
       onEnter: (e) => _mouseEnter(true),
       onExit: (e) => _mouseEnter(false),
       child: AnimatedBuilder(
-        animation: _controller,
-        builder: (context, child) => Transform(
-          alignment: Alignment.center,
-          child: widget.child,
-          transform: _hovering ? _hoverTransform : _nonHoverTransform,
-        ),
+        animation: _animation,
+        builder: (BuildContext context, Widget child) {
+          _hovering ? _controller.forward() : _controller.reverse();
+          return Transform.scale(
+            scale: (_animationCurve.value * 1.15) + 1,
+            child: widget.child,
+          );
+        },
       ),
     );
   }
